898e295a87caf8c1f899fcf454440143dbf50378
Eric Lau <ericlaucmo@gmail.com>
2020-05-08 22:31:51 -0700

    Lesson 7

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -3,11 +3,11 @@ import Link from 'next/link';
 const Nav = () => {
   return (
     <div>
-      <Link href="/sell">
-        <a>Sell</a>
-      </Link>
       <Link href="/">
         <a>Home</a>
+      </Link>
+      <Link href="/sell">
+        <a>Sell</a>
       </Link>
     </div>
   );
