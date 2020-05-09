d9d339b472bd56514ffeee66b2fb891b0509807a
Eric Lau <ericlaucmo@gmail.com>
2020-05-09 09:12:48 -0700

    Lesson 15

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,25 @@
-const Mutations = {};
+const Mutations = {
+  createItem: async (parent, args, ctx, info) => {
+    // TODO: Check if they are logged in
+
+    /**
+     * ctx comes from createServer context
+     * ctx contains all things inside the request and the database connection
+     * DB connection automatically has all the methods inside prisma.graphql
+     * can instead import the db, instead of passing it into the context
+     * passing "info" as the second argument will make sure the item being created
+     *   is returned from the database
+     */
+
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: { ...args }
+      },
+      info
+    );
+
+    return item;
+  }
+};
 
 module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,18 @@
-const Query = {};
+/**
+ * forwardTo can be used when the query is exact same on Yoga and Prisma
+ * However, there is no custom logic
+ *    no auth, etc
+ *
+ */
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+  items: forwardTo('db')
+
+  // items: async (parent, args, ctx, info) => {
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // }
+};
 
 module.exports = Query;
